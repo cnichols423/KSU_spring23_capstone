c81076540e7b1083170d0a5d5c3856c2d1543e89
jfried16 <74629287+jfried16@users.noreply.github.com>
2023-03-08 19:51:10 -0500

    Changes to add sprint
    Beginning of the sprint implementation

--- EducationThroughPlay/Assets/Stylized Astronaut/Character/Player.cs
+++ EducationThroughPlay/Assets/Stylized Astronaut/Character/Player.cs
@@ -6,24 +6,47 @@ public class Player : MonoBehaviour {
 		private Animator anim;
 		private CharacterController controller;
 
-		public float speed = 600.0f;
+		public float speed = 200f;
 		public float turnSpeed = 400.0f;
 		private Vector3 moveDirection = Vector3.zero;
 		public float gravity = 20.0f;
+        //var tmp = Player.anim.speed;
 
-		void Start () {
+    void Start () {
 			controller = GetComponent <CharacterController>();
 			anim = gameObject.GetComponentInChildren<Animator>();
 		}
 
 		void Update (){
 			if (Input.GetKey ("w")) {
-				anim.SetInteger ("AnimationPar", 1);
-			}  else {
+                if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    anim.SetInteger("AnimationPar", 1);
+                }
+			}  
+            else {
 				anim.SetInteger ("AnimationPar", 0);
 			}
 
-			if(controller.isGrounded){
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+            speed = 400f;
+            anim.SetInteger("AnimationPar", 2);
+        }
+            else
+            {
+            speed = 200f;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+        else
+        {
+            
+        }
+
+        if (controller.isGrounded){
 				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
 			}
 
