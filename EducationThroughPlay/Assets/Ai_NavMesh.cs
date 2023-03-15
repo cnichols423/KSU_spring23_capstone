a13bdfb14cc13b691c7072a13329535d1f0ad57d
Cohen Nichols <cnicho35@kent.edu>
2023-03-15 19:38:43 -0400

    AI/level layout start
    I started on the layout of my level and adding some AI movement in it. only added 1 little area for the AI or player to move on. and the AI does not move yet.

---
+++ EducationThroughPlay/Assets/Ai_NavMesh.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// Nav mesh agent by Cohen Nichols
+/*
+public class PlayerNavMesh : MonoBehaviour
+{
+   
+   [SerializeField] private Transform movePositionTransform; 
+
+   private NavMeshAgent navMeshAgent;
+   
+    // Awake is called before the first frame update
+    private void Awake(){
+        navMeshAgent = GetComponet<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        navMeshAgent.destination = movePositionTransform.posistion;
+    }
+}
+*/
