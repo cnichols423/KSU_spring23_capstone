abd278e9901a40f37e4c4a8f89d0a2dc23f39d27
Jason <34782511+JAbo101@users.noreply.github.com>
2023-03-15 04:18:37 -0400

    good morning

--- EducationThroughPlay/Assets/Stylized Astronaut/Character/Player.cs
+++ EducationThroughPlay/Assets/Stylized Astronaut/Character/Player.cs
@@ -7,21 +7,66 @@ public class Player : MonoBehaviour
 
     private Animator anim;
     private CharacterController controller;
+    private Rigidbody rb;
+
 
     public float speed = 20f;
     public float turnSpeed = 400.0f;
-    private Vector3 moveDirection = Vector3.zero;
+    
     public float gravity = 20.0f;
+
+
+    public float jump = 5.0f;
+    bool isJumping;
+    float jumpTemp;
+
+    private Vector3 moveDirection = Vector3.zero;
     //var tmp = Player.anim.speed;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
         anim = gameObject.GetComponentInChildren<Animator>();
+        rb = GetComponentInChildren<Rigidbody>();
     }
 
     void Update()
     {
+        Debug.Log(jumpTemp);
+        movement();
+
+        // Implementation of Jump
+        //on the ground and space is pressed
+        if (Input.GetKey(KeyCode.Space) && controller.isGrounded)
+        {
+            anim.SetInteger("AnimationPar", 3);
+            isJumping = true;
+            jumpTemp = 0.0f;
+
+        }
+        if (!Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            //anim.SetInteger("Jump Start", 0);
+        }
+
+        if (controller.isGrounded && !Input.GetKey("w") && !Input.GetKey(KeyCode.Space))
+        {
+            anim.SetInteger("AnimationPar", 0);
+        }
+
+        if(isJumping) {
+            jumpTemp = Mathf.Lerp(jumpTemp, jump, Time.deltaTime);
+            controller.Move(transform.up * jumpTemp);
+
+            if(jumpTemp >= .1) {
+                isJumping = false; 
+                jumpTemp = 0.0f;
+            }
+        }
+    }
+
+    void movement() {
+
         // Implementation of walk
         if (Input.GetKeyDown("w"))
         {
@@ -46,26 +91,10 @@ public class Player : MonoBehaviour
         {
             moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
         }
-
+        
         float turn = Input.GetAxis("Horizontal");
         transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
         controller.Move(moveDirection * Time.deltaTime);
         moveDirection.y -= gravity * Time.deltaTime;
-
-        // Implementation of Jump
-        if (Input.GetKey(KeyCode.Space) && controller.isGrounded)
-        {
-            anim.SetInteger("AnimationPar", 3);
-            transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
-        }
-        if (!Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
-        {
-            //anim.SetInteger("Jump Start", 0);
-        }
-
-        if (controller.isGrounded && !Input.GetKey("w") && !Input.GetKey(KeyCode.Space))
-        {
-            anim.SetInteger("AnimationPar", 0);
-        }
     }
 }
