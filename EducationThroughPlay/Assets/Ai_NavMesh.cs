ea75a180b8715f5f4fb90a2f3eaf5ebb7ae6e7c9
Cohen Nichols <cnicho35@kent.edu>
2023-04-05 14:56:36 -0400

    rework of AI
    reworked the Ai_NavMesh code for level 6. the AI agent now moves to the correct spot on scene start

--- EducationThroughPlay/Assets/Ai_NavMesh.cs
+++ EducationThroughPlay/Assets/Ai_NavMesh.cs
@@ -1,25 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+// Nav mesh agent by Cohen Nichols
+/* 
+
+In this script, we first declare a public variable target of type Transform, 
+which represents the location that we want the AI navmesh to move towards.
+
+*/
+
 using UnityEngine;
 using UnityEngine.AI;
 
-// Nav mesh agent by Cohen Nichols
-/*
-public class PlayerNavMesh : MonoBehaviour
+
+public class Ai_NavMesh : MonoBehaviour
 {
    
-   [SerializeField] private Transform movePositionTransform; 
+   public Transform target; 
 
-   private NavMeshAgent navMeshAgent;
+   private NavMeshAgent navAgent;
    
-    // Awake is called before the first frame update
-    private void Awake(){
-        navMeshAgent = GetComponet<NavMeshAgent>();
+    // Start is called before the first frame update
+    /*
+
+    In the Start() method, we get a reference to the NavMeshAgent component 
+    on the game object by calling GetComponent<NavMeshAgent>().
+
+    */
+    void Start(){
+        navAgent = GetComponent<NavMeshAgent>(); // get the nav mesh agent component
     }
+
+
 
     // Update is called once per frame
-    private void Update() {
-        navMeshAgent.destination = movePositionTransform.posistion;
+    /*
+    
+    In the Update() method, we call navAgent.SetDestination(target.position) to set 
+    the destination of the NavMeshAgent to the target position. 
+
+    */
+    void Update() {
+        navAgent.SetDestination(target.position); // set the destination for the nav mesh agent
     }
 }
-*/
