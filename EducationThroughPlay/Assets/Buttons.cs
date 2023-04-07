da0b9676ad4fa243d76ea76512cf27532eec194f
jfried16 <74629287+jfried16@users.noreply.github.com>
2023-04-06 20:15:06 -0400

    Lesson 4 improvements
    Improved UI (it is still broken), added a glass material and redid the buttons to the design that will be used in the final product

--- EducationThroughPlay/Assets/Buttons.cs
+++ EducationThroughPlay/Assets/Buttons.cs
@@ -1,29 +1,26 @@
+// Joshua Friedman
+// Education Through Play
+// Capstone Spring 2023
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
+    // Initialize a variable to hold the UI element to display (this is set in the Unity Editor on a per item basis)
     [SerializeField] private GameObject containerGameObject;
 
+    // Function to show the UI element
     private void Show()
     {
+        // Set the UI element to active
         containerGameObject.SetActive(true);
     }
 
+    // Function that gets called by "PlayerInteract"
     public void Interact()
     {
+        // When the User interacts with the object show the associated UI element
         Show();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- EducationThroughPlay/Assets/PlayerInteract.cs
+++ EducationThroughPlay/Assets/PlayerInteract.cs
@@ -1,37 +1,36 @@
+// Joshua Friedman
+// Education Through Play
+// Capstone Spring 2023
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        // A float that controls the range of player interaction
+        float interactRange = 3f;
+
         // When the player presses "E"
         if (Input.GetKeyDown(KeyCode.E))
         {
-            // A float that controls the range of player interaction
-            float interactRange = 2f;
-
             // Make a collider array and store every object that is overlapping in an array
             Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
             // For each collider in that array
             foreach (Collider collider in colliderArray)
             {
-                //
+                // If the game object associated with the collider has the "Buttons" script attached
                 if (collider.TryGetComponent(out Buttons button))
                 {
+                    // Call the interact function in the "Buttons" script
                     button.Interact();
                 }
             }
-
+            // End of loop
         }
+        // End of if statement
     }
+
 }
