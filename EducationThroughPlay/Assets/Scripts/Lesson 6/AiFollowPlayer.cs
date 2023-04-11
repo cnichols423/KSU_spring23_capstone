b59c85b7cafef310869646b34d058861730a5f1a
Cohen Nichols <cnicho35@kent.edu>
2023-04-11 16:23:45 -0400

    added meldins map
    Added meldins map to the files. also started the final design for lesson 6

---
+++ EducationThroughPlay/Assets/Scripts/Lesson 6/AiFollowPlayer.cs
@@ -0,0 +1,33 @@
+/* Nav mesh agent by Cohen Nichols
+
+In this script, we first declare a public game object for the player model
+then use the unity ai to create the nav mesh agent object we want to make move
+
+
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class AiFollowPlayer : MonoBehaviour
+{
+    public GameObject playerModel;
+    private NavMeshAgent aiAgent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aiAgent.destination = playerModel.transform.position;
+    }
+}
